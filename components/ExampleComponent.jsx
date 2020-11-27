484d7c81cf69bb2053e28a7aeeadec8b9a6bf4f7
Dm1tr1y147 <Dm1tr1y147@users.noreply.github.com>
2020-11-27 17:41:31 +0000

    Apply Prettier styles

--- components/ExampleComponent.jsx
+++ components/ExampleComponent.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useCounter } from "hooks/counter"
+import React from "react";
+import { useCounter } from "hooks/counter";
 
 import styles from "styles/exampleComponent.module.css";
 
 const ExampleComponent = () => {
-  const {} = useCounter()
+  const {} = useCounter();
   return <button className={styles.button}>Amazing button by Timothy!</button>;
 };
 
